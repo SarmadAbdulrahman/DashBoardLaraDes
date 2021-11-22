982e56b195f20c0aa824d2428ac4b86ea43d1191
SarmadAbdulrahman <66552903+SarmadAbdulrahman@users.noreply.github.com>
2021-11-22 13:35:34 +0300

    20211416cm

---
+++ resources/views/layouts/Header.blade.php
@@ -0,0 +1,45 @@
+
+<!DOCTYPE html>
+<!--
+Restx
+Version: 1.6.0
+Date:2021-11-22 11:41 AM
+-->
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<!-- Head -->
+<head>
+    <meta charset="utf-8" />
+    <title>PageName</title>
+
+    <meta name="description" content="blank page" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="shortcut icon" href="assets/img/favicon.png')}}" type="image/x-icon">
+
+    <!--Basic Styles-->
+    <link href="{{url('assets/css/bootstrap.min.css')}}" rel="stylesheet" />
+    <link id="bootstrap-rtl-link" href="" rel="stylesheet" />
+    <link href="{{url('assets/css/font-awesome.min.css')}}" rel="stylesheet" />
+    <link href="{{url('assets/css/weather-icons.min.css')}}" rel="stylesheet" />
+
+  
+
+    <!--Beyond styles-->
+    <link id="beyond-link" href="{{url('assets/css/beyond.min.css')}}" rel="stylesheet" />
+    <link href="{{url('assets/css/demo.min.css')}}" rel="stylesheet" />
+    <link href="{{url('assets/css/typicons.min.css')}}" rel="stylesheet" />
+    <link href="{{url('assets/css/animate.min.css')}}" rel="stylesheet" />
+    <link id="skin-link" href="" rel="stylesheet" type="text/css" />
+
+    <!--Skin Script: Place this script in head to load scripts for skins and rtl support-->
+    <script src="{{url('assets/js/skins.min.js')}}"></script>
+</head>
+@yield('SideBar')
+@yield('content')
+<!-- Main Container -->
+@yield('Footer')
+<!-- /Head -->
+<!-- Body -->
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,16 @@
+
+    <!--Basic Scripts-->
+    @section('Footer')
+    <script src="{{url('assets/js/jquery.min.js')}}"></script>
+    <script src="{{url('assets/js/bootstrap.min.js')}}"></script>
+    <script src="{{url('assets/js/slimscroll/jquery.slimscroll.min.js')}}"></script>
+
+    <!--Beyond Scripts-->
+    <script src="{{url('assets/js/beyond.min.js')}}"></script>
+
+    <!--Page Related Scripts-->
+
+</body>
+<!--  /Body -->
+</html>
+@endsection
